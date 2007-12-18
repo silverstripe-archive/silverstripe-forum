f312884b763ee20072d18c7efa66677da9675a17
Sam Minnee <sam@silverstripe.com>
2007-12-18 02:17:54 +0000

    Small fix to allow the forum & ecommerce modules to coexist

--- code/ForumRole.php
+++ code/ForumRole.php
@@ -150,7 +150,7 @@ class ForumRole extends DataObjectDecorator {
 		if(Permission::checkMember($this->owner->ID, "ACCESS_FORUM")) {
 			$fields->insertBefore(new TextField("Nickname", "Nickname"), "FirstName");
 			$fields->insertAfter(new TextField("Occupation", "Occupation"), "Surname");
-			$fields->insertAfter(new CountryDropdownField("Country", "Country"), "Occupation");
+			if(!$fields->fieldByName('Country')) $fields->insertAfter(new CountryDropdownField("Country", "Country"), "Occupation");
 
 			$fields->insertAfter(new PasswordField("ConfirmPassword", "Confirm Password"), "Password");
 			$fields->push(new ImageField("Avatar", "Upload avatar"));
