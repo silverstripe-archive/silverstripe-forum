655b9f3dadaeac8ec48f3f4725b6b605eaa28c32
Sam Minnee <sam@silverstripe.com>
2008-02-22 00:49:00 +0000

    Fixed deprecation error

--- code/Forum.php
+++ code/Forum.php
@@ -152,7 +152,7 @@ class Forum extends Page {
 		$nonPageParts = array();
 		$parts = array();
 
-		$controller = Controller::currentController();
+		$controller = Controller::curr();
 		$params = $controller->getURLParams();
 
 		$SQL_id = $params['ID'];
