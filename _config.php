e26b7fdd9fff7a185d54a27d6ac93ba4e3a17a0b
Sean Harvey <sean@silverstripe.com>
2009-02-12 08:56:50 +0000

    MINOR Removed addPermissions() which didn't work because permissions are set when MemberTableField is instanstiated. addPermissions() is now deprecated on MemberTableField

--- _config.php
+++ _config.php
@@ -1,22 +1,16 @@
 <?php
 
-/* Put here all the function calls to update the link with the Member table */
-
 DataObject::add_extension('Member', 'ForumRole');
 
 Object::add_extension('Member_Validator', 'ForumRole_Validator');
 
-MemberTableField::addPermissions( array( "show", "export" ) );
-
-MemberTableField::addMembershipFields( array(
+MemberTableField::addMembershipFields(array(
 	"Nickname" => "Nickname",
 	"Occupation" => "Occupation",
 	"Country" => "Country",
 	"ForumRank" => "ForumRank"
-	));
+));
 
 BBCodeParser::enable_smilies();
-
-//File::setMaxFileSize("100000", "100000", '*');
 
 ?>
