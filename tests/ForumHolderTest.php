ca44cb2a49a55d1f6467674d62799f6a558003dc
Geoff Munn <geoff@silverstripe.com>
2010-04-20 04:49:53 +0000

    BUGFIX: tests now have default sort enforced

--- tests/ForumHolderTest.php
+++ tests/ForumHolderTest.php
@@ -24,8 +24,8 @@ class ForumHolderTest extends FunctionalTest {
 
 		// Test what we got back from the two categories. The first expects 2 forums, the second
 		// expects none.
-		$this->assertTrue($fh_controller->Categories()->First()->Forums()->Count() == 2, "fh first category has 2 forums");
-		$this->assertTrue($fh_controller->Categories()->Last()->Forums()->Count() == 0, "fh second category has 0 forums");
+		$this->assertTrue($fh_controller->Categories()->First()->Forums()->Count() == 0, "fh first category has 0 forums");
+		$this->assertTrue($fh_controller->Categories()->Last()->Forums()->Count() == 2, "fh second category has 2 forums");
 		
 		// Test ForumHolder::Categories() on 'fh2', from which we expect 2 categories
 		$fh2 = $this->objFromFixture("ForumHolder", "fh2");
