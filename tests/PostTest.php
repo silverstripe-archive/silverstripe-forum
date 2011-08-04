silverstripe-archive/silverstripe-forum

commit d58cdbcb477e1c0812a0357aed67d53d6a469552

APICHANGE: rewrite the forum permissions

code/Post.php
@@ -85,38 +85,35 @@ class Post extends DataObject {
 	}
 
 	/**
-	 * Return whether we can edit this post. Only the user, moderator
-	 * or admin can edit post
-	 *
-	 * @return bool
-	 */
-	function canEdit() {
-		if($member = Member::currentUser()) {
-			if($member->ID == $this->AuthorID) return true;
-		}
-
-		return $this->Thread()->canEdit();
-	}
-	
-	/**
-	 * Return whether we can delete this post. If a user can edit
-	 * a post then they can delete the post
-	 *
-	 * @return bool
-	 */
-	function canDelete() {
-		return $this->canEdit();
-	}
-	
-	/**
-	 * Return whether we can view this post. Needs to check the forums can
-	 * view since an individual posts won't be 'hidden' but whole forums will
-	 * be. Readonly posts aren't supported but read only threads are
-	 *
-	 * @return bool
+	 * Check if user can see the post
 	 */
 	function canView() {
 		return $this->Thread()->canView();
+	}
+
+	/**
+	 * Check if user can edit the post (only if it's his own)
+	 */
+	function canEdit() {
+		if($member = Member::currentUser()) {
+			if($this->Thread()->canPost() && $member->ID==$this->AuthorID) return true;
+		}
+
+		return false;
+	}
+	
+	/**
+	 * Check if user can delete this post - only moderators are allowed to delete.
+	 */
+	function canDelete() {
+		return $this->Thread()->canModerate();
+	}
+	
+	/**
+	 * Check if user can add new posts - hook up into canPost.
+	 */
+	function canCreate() {
+		return $this->Thread()->canPost();
 	}
 	
 	/**

tests/ForumTest.yml
@@ -65,6 +65,8 @@ ForumHolder:
 	fhPermissions:
 		Title: Forum For Testing Threads with Permissions
 		ShowInCategories: true
+		CanViewType: NoOne
+		CanPostType: NoOne
 ForumCategory:
 	fh1cat1:
 		Title: cat1
@@ -87,6 +89,8 @@ Forum:
 		Title: General Discussion
 		ParentID: =>ForumHolder.fh
 		CategoryID: =>ForumCategory.fh1cat1
+		CanViewType: Anyone
+		CanPostType: Anyone
 		CanAttachFiles: true
 		Moderators: =>Member.moderator
         
@@ -114,20 +118,31 @@ Forum:
 		Title: Limited to Logged In Posting and Viewing
 		ParentID: =>ForumHolder.fhPermissions
 		CanViewType: LoggedInUsers
-		ForumPosters: LoggedInUsers
+		CanPostType: LoggedInUsers
+		Moderators: =>Member.moderator
         
 	limitedToGroup:
 		Title: Limited to a Specific Group
 		ParentID: =>ForumHolder.fhPermissions
 		CanViewType: OnlyTheseUsers
-		ForumPosters: OnlyTheseUsers
+		CanPostType: OnlyTheseUsers
 		ViewerGroups: =>Group.special
 		PosterGroups: =>Group.special
+		Moderators: =>Member.moderator
         
 	noPostingForum:
 		Title: This Forum is Readonly
 		ParentID: =>ForumHolder.fhPermissions
-		ForumPosters: NoOne
+		CanViewType: Anyone
+		CanPostType: NoOne
+		Moderators: =>Member.moderator
+
+	inheritedForum:
+		Title: This Forum is Readonly
+		ParentID: =>ForumHolder.fhPermissions
+		CanViewType: Inherit
+		CanPostType: Inherit
+		Moderators: =>Member.moderator
     
 ForumThread:
 	Thread1:
@@ -158,6 +173,10 @@ ForumThread:
 	ThreadWhichIsInReadonlyForum:
 		Title: Thread is set to a readonly forum but you can edit this because its not readonly
 		Forum: =>Forum.noPostingForum
+
+	ThreadWhichIsInInheritedForum:
+		Title: The thread is in a forum that inherits NoOne permission for viewing and posting
+		Forum: =>Forum.inheritedForum
 		
 	ThreadCreatedBySpammer:
 		Title: Thread Created By Spammer
@@ -304,9 +323,16 @@ Post:
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
+
+	ReadonlyThreadPost:
+		Content: ReadonlyThreadPost
+		Thread: =>ForumThread.ReadonlyThread
+		Author: =>Member.test1
+		Forum: =>Forum.general
             
 	Post18:
 		Content: This is the last post to a long thread
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
+

tests/PostTest.php
@@ -3,6 +3,43 @@
 class PostTest extends FunctionalTest {
 	
 	static $fixture_file = "forum/tests/ForumTest.yml";
+
+	function testPermissions() {
+		$member = $this->objFromFixture('Member', 'test1');
+		$this->session()->inst_set('loggedInAs', $member->ID);
+
+		// read only thread post
+		$readonly = $this->objFromFixture('Post', 'ReadonlyThreadPost');
+		$this->assertFalse($readonly->canEdit()); // Even though it's user's own
+		$this->assertTrue($readonly->canView());
+		$this->assertFalse($readonly->canCreate());
+		$this->assertFalse($readonly->canDelete());
+		
+		// normal thread. They can post to these
+		$post = $this->objFromFixture('Post', 'Post18');
+		$this->assertFalse($post->canEdit()); // Not user's post
+		$this->assertTrue($post->canView());
+		$this->assertTrue($post->canCreate());
+		$this->assertFalse($post->canDelete());
+		
+		$member = $this->objFromFixture('Member', 'test2');
+		$this->session()->inst_set('loggedInAs', $member->ID);
+
+		// Check the user can edit his own post (but not delete)
+		$this->assertTrue($post->canEdit()); // User's post
+		$this->assertTrue($post->canView());
+		$this->assertTrue($post->canCreate());
+		$this->assertFalse($post->canDelete());
+
+		// Moderator can delete posts
+		$member = $this->objFromFixture('Member', 'moderator');
+		$member->logIn();
+
+		$this->assertFalse($post->canEdit());
+		$this->assertTrue($post->canView());
+		$this->assertTrue($post->canCreate());
+		$this->assertTrue($post->canDelete());
+	}
 	
 	function testGetTitle() {
 		$post = $this->objFromFixture('Post', 'Post1');
@@ -81,10 +118,11 @@ class PostTest extends FunctionalTest {
 		// should be false since we're not logged in.
 		if($member = Member::currentUser()) $member->logOut();
 		
-		$this->assertFalse($post->EditLink());		
+		$this->assertFalse($post->EditLink());
+		$this->assertFalse($post->DeleteLink());
 		
-		// logged in as the member. Should be able to delete it
-		$member = $this->objFromFixture('Member', 'test1');
+		// logged in as the moderator. Should be able to delete the post.
+		$member = $this->objFromFixture('Member', 'moderator');
 		$member->logIn();
 		
 		$this->assertContains($post->Thread()->URLSegment .'/deletepost/'. $post->ID, $post->DeleteLink());

code/Forum.php
@@ -17,7 +17,7 @@ class Forum extends Page {
 
 	static $db = array(
 		"Abstract" => "Text",
-		"ForumPosters" => "Enum('Anyone, LoggedInUsers, OnlyTheseUsers, NoOne', 'LoggedInUsers')",
+		"CanPostType" => "Enum('Inherit, Anyone, LoggedInUsers, OnlyTheseUsers, NoOne', 'LoggedInUsers')",
 		"CanAttachFiles" => "Boolean",
 	);
 
@@ -52,36 +52,31 @@ class Forum extends Page {
 	static $redirect_post_urls_to_thread = false;
 
 	/**
-	 * Can this user view the thread.
-	 *
-	 * @return bool
+	 * Check if the user can view the forum.
 	 */
 	function canView() {
-		return (parent::canView() || $this->isAdmin());
+		return (parent::canView() || $this->canModerate());
 	}
 	
 	/**
-	 * Can the user edit this thread - The settings and configuration in the thread.
-	 * Not the individual posts. Individual posts is controlled by canCreate
-	 *
-	 * @return bool
-	 */
-	function canEdit() {
-		return $this->isAdmin();
-	}
-	
-	/**
-	 * Can the user post threads to this forum
-	 *
-	 * @return bool
+	 * Check if the user can post to the forum and edit his own posts.
 	 */
 	function canPost() {
-		if($this->ForumPosters == "Anyone" || $this->isAdmin()) return true;
+		if($this->CanPostType == "Inherit") {
+			$holder = $this->getForumHolder();
+			if ($holder) {
+				return $holder->canPost();
+			}
+
+			return false;
+		}
+
+		if($this->CanPostType == "Anyone" || $this->canEdit()) return true;
 		
-		if($this->ForumPosters == "NoOne") return false;
+		if($this->CanPostType == "NoOne") return false;
 		
 		if($member = Member::currentUser()) {
-			if($this->ForumPosters == "LoggedInUsers") return true;
+			if($this->CanPostType == "LoggedInUsers") return true;
 
 			if($groups = $this->PosterGroups()) {
 				foreach($groups as $group) {
@@ -90,6 +85,22 @@ class Forum extends Page {
 			}
 		}
 		
+		return false;
+	}
+
+	/**
+	 * Check if user has access to moderator panel and can delete posts and threads.
+	 */
+	function canModerate() {
+		if(!Member::currentUserID()) return false;
+		
+		$member = Member::currentUser();
+		
+		// Admins
+		if ($this->canEdit()) return true; 
+		// Moderators
+		if ($member->isModeratingForum($this)) return true;
+
 		return false;
 	}
 	
@@ -101,67 +112,6 @@ class Forum extends Page {
 	 */
 	function canAttach() {
 		return $this->CanAttachFiles ? true : false;
-	}
-	
-	/**
-	 * Add default records to database
-	 *
-	 * This function is called whenever the database is built, after the
-	 * database tables have all been created.
-	 */
-	public function requireDefaultRecords() {
-		parent::requireDefaultRecords();
-
-		$code = "ACCESS_FORUM";
-
-		if(!$forumGroup = DataObject::get_one("Group", "\"Group\".\"Code\" = 'forum-members'")) {
-			$group = new Group();
-			$group->Code = 'forum-members';
-			$group->Title = "Forum Members";
-			$group->write();
-
-			Permission::grant( $group->ID, $code );
-			DB::alteration_message(_t('Forum.GROUPCREATED','Forum Members group created'),"created"); 
-		}
-		else if(DB::query("SELECT * FROM \"Permission\" WHERE \"GroupID\" = '$forumGroup->ID' AND \"Code\" LIKE '$code'")->numRecords() == 0 ) {
-			Permission::grant($forumGroup->ID, $code);
-		}
-		if(!$category = DataObject::get_one("ForumCategory")) {
-			$category = new ForumCategory();
-			$category->Title = _t('Forum.DEFAULTCATEGORY', 'General');
-			$category->write();
-		}
-		if(!DataObject::get_one("ForumHolder")) {
-			$forumholder = new ForumHolder();
-			$forumholder->Title = "Forums";
-			$forumholder->URLSegment = "forums";
-			$forumholder->Content = "<p>"._t('Forum.WELCOMEFORUMHOLDER','Welcome to SilverStripe Forum Module! This is the default ForumHolder page. You can now add forums.')."</p>";
-			$forumholder->Status = "Published";
-			$forumholder->write();
-			$forumholder->publish("Stage", "Live");
-			DB::alteration_message(_t('Forum.FORUMHOLDERCREATED','ForumHolder page created'),"created");
-			$forum = new Forum();
-			$forum->Title = _t('Forum.TITLE','General Discussion');
-			$forum->URLSegment = "general-discussion";
-			$forum->ParentID = $forumholder->ID;
-			$forum->Content = "<p>"._t('Forum.WELCOMEFORUM','Welcome to SilverStripe Forum Module! This is the default Forum page. You can now add topics.')."</p>";
-			$forum->Status = "Published";
-			$forum->CategoryID = $category->ID;
-			$forum->write();
-			$forum->publish("Stage", "Live");
-
-			DB::alteration_message(_t('Forum.FORUMCREATED','Forum page created'),"created");
-		}
-		
-		// update the forumpostersgroupID
-		if($this->ForumPostersGroupID > 0) {
-			$this->PosterGroups()->add(DataObject::get_by_id('Group', $this->ForumPostersGroupID));
-			
-			$this->ForumPostersGroupID == 0;
-			$this->write();
-			
-			DB::alteration_message(_t('Forum.FORUMPOSTERSGROUPMIGRATED','Forum posters group migrated'),"created");
-		}
 	}
 
 	/**
@@ -176,7 +126,8 @@ class Forum extends Page {
 	  	$fields = parent::getCMSFields();
 	
 		$fields->addFieldToTab("Root.Access", new HeaderField(_t('Forum.ACCESSPOST','Who can post to the forum?'), 2));
-		$fields->addFieldToTab("Root.Access", new OptionsetField("ForumPosters", "", array(
+		$fields->addFieldToTab("Root.Access", new OptionsetField("CanPostType", "", array(
+			"Inherit" => "Inherit",
 		  	"Anyone" => _t('Forum.READANYONE', 'Anyone'),
 		  	"LoggedInUsers" => _t('Forum.READLOGGEDIN', 'Logged-in users'),
 		  	"OnlyTheseUsers" => _t('Forum.READLIST', 'Only these people (choose from list)'),
@@ -228,26 +179,6 @@ class Forum extends Page {
 		return $fields;
 	}
 
-	/**
-	 * Return true if user is an "admin" of this forum or is a moderator.
-	 * The user can either have ADMIN permissions {@link Permission} or be
-	 * a moderator of this forum (their member ID is in the Moderators
-	 * many many relation ID list).
-	 * 
-	 * @see ForumRole->isModeratingForum()
-	 * 
-	 * @return boolean
-	 */
-	function isAdmin() {
-		if(!Member::currentUserID()) return false;
-		
-		$member = Member::currentUser();
-		
-		$isModerator = ($member) ? $member->isModeratingForum($this) : false;
-
-		return (Permission::check('ADMIN') || $isModerator) ? true : false;
-	}
-	
 	/**
 	 * Create breadcrumbs
 	 *
@@ -305,7 +236,8 @@ class Forum extends Page {
 	 * @return ForumHolder
 	 */
 	function getForumHolder() {
-		return $this->Parent();
+		$holder = $this->Parent();
+		if ($holder->ClassName=='ForumHolder') return $holder;
 	}
 
 	/**
@@ -547,7 +479,7 @@ class Forum_Controller extends Page_Controller {
 	 * Must be logged in and have the correct permissions to do marking
 	 */
 	function markasspam() {
-		if($this->isAdmin() && isset($this->urlParams['ID'])) {
+		if($this->canModerate() && isset($this->urlParams['ID'])) {
 			$post = DataObject::get_by_id('Post', $this->urlParams['ID']);
 			
 			if($post) {	
@@ -630,25 +562,36 @@ class Forum_Controller extends Page_Controller {
 	 * @return Form Returns the post message form
 	 */
 	function PostMessageForm($addMode = false, $post = false) {
-		
 		$thread = false;
 		
 		if($post) $thread = $post->Thread();
 		else if(isset($this->urlParams['ID'])) $thread = DataObject::get_by_id('ForumThread', $this->urlParams['ID']);	
-		
-		// Check to see that the user has create forum thread rights
-		if(!$this->canPost()) {
-			$messageSet = array(
-				'default' => _t('Forum.LOGINTOPOST','You\'ll need to login before you can post to that forum. Please do so below.'),
-				'alreadyLoggedIn' => _t('Forum.LOGINTOPOSTLOGGEDIN','I\'m sorry, but you can\'t post to this forum until you\'ve logged in.  If you want to log in as someone else, do so below. If you\'re logged in and you still can\'t post, you don\'t have the correct permissions to post.'),
-				'logInAgain' => _t('Forum.LOGINTOPOSTAGAIN','You have been logged out of the forums.  If you would like to log in again to post, enter a username and password below.'),
-			);
-			
- 			Security::permissionFailure($this, $messageSet);
 
-			return false;
+		// Check permissions
+		$messageSet = array(
+			'default' => _t('Forum.LOGINTOPOST','You\'ll need to login before you can post to that forum. Please do so below.'),
+			'alreadyLoggedIn' => _t('Forum.LOGINTOPOSTLOGGEDIN','I\'m sorry, but you can\'t post to this forum until you\'ve logged in.  If you want to log in as someone else, do so below. If you\'re logged in and you still can\'t post, you don\'t have the correct permissions to post.'),
+			'logInAgain' => _t('Forum.LOGINTOPOSTAGAIN','You have been logged out of the forums.  If you would like to log in again to post, enter a username and password below.'),
+		);
+		
+		// Creating new thread
+		if ($addMode && !$this->canPost()) {
+ 			Security::permissionFailure($this, $messageSet);
+			return false;			
 		}
 
+		// Replying to existing thread
+		if (!$addMode && !$post && !$thread->canPost()) {
+ 			Security::permissionFailure($this, $messageSet);
+			return false;			
+		}
+
+		// Editing existing post
+		if (!$addMode && $post && !$post->canEdit()) {
+ 			Security::permissionFailure($this, $messageSet);
+			return false;			
+		}
+		
 		$fields = new FieldSet(
 			($post && $post->isFirstPost() || !$thread) ? new TextField("Title", _t('Forum.FORUMTHREADTITLE', 'Title')) : new ReadonlyField('Title',  _t('Forum.FORUMTHREADTITLE', 'Title'), 'Re:'. $thread->Title),
 			new TextareaField("Content", _t('Forum.FORUMREPLYCONTENT', 'Content')),
@@ -724,34 +667,13 @@ class Forum_Controller extends Page_Controller {
 		// If a thread id is passed append the post to the thread. Otherwise create
 		// a new thread
 		$thread = false;
-		
 		if(isset($data['ThreadID'])) {
 			$thread = DataObject::get_by_id('ForumThread', $data['ThreadID']);
-		}
-		if(!$thread) {
-			$thread = new ForumThread();
-			$thread->ForumID = $this->ID;
-			if($title) $thread->Title = $title;
-		}
-		
-		// check permissions. even if this is a reply we can check canCreate since if a thread
-		// is readonly then they cannot reply to it.
-		if(!$thread->canCreate()) {
-			$messageSet = array(
-				'default' => _t('Forum.LOGINTOPOST','You\'ll need to login before you can post to that forum. Please do so below.'),
-				'alreadyLoggedIn' => _t('Forum.NOPOSTPERMISSION','I\'m sorry, but you do not have permission post to this forum.'),
-				'logInAgain' => _t('Forum.LOGINTOPOSTAGAIN','You have been logged out of the forums.  If you would like to log in again to post, enter a username and password below.'),
-			);
-
-			Security::permissionFailure($this, $messageSet);
-			
-			return false;
 		}
 
 		// If this is a simple edit the post then handle it here. Look up the correct post,
 		// make sure we have edit rights to it then update the post
 		$post = false;
-		
 		if(isset($data['ID'])) {
 			$post = DataObject::get_by_id('Post', $data['ID']);
 			
@@ -760,6 +682,38 @@ class Forum_Controller extends Page_Controller {
 					$thread->Title = $title;
 				}
 			}
+		}
+
+
+		// Check permissions
+		$messageSet = array(
+			'default' => _t('Forum.LOGINTOPOST','You\'ll need to login before you can post to that forum. Please do so below.'),
+			'alreadyLoggedIn' => _t('Forum.NOPOSTPERMISSION','I\'m sorry, but you do not have permission post to this forum.'),
+			'logInAgain' => _t('Forum.LOGINTOPOSTAGAIN','You have been logged out of the forums.  If you would like to log in again to post, enter a username and password below.'),
+		);
+		
+		// Creating new thread
+		if (!$thread && !$this->canPost()) {
+ 			Security::permissionFailure($this, $messageSet);
+			return false;			
+		}
+
+		// Replying to existing thread
+		if ($thread && !$post && !$thread->canPost()) {
+ 			Security::permissionFailure($this, $messageSet);
+			return false;			
+		}
+
+		// Editing existing post
+		if ($thread && $post && !$post->canEdit()) {
+ 			Security::permissionFailure($this, $messageSet);
+			return false;			
+		}
+
+		if(!$thread) {
+			$thread = new ForumThread();
+			$thread->ForumID = $this->ID;
+			if($title) $thread->Title = $title;
 		}
 		
 		// from now on the user has the correct permissions. save the current thread settings
@@ -886,6 +840,7 @@ class Forum_Controller extends Page_Controller {
 			$title = Convert::raw2xml($thread->Title) . ' &raquo; ' . $title;
 			
 			return array(
+				'Thread' => $thread,
 				'Title' => DBField::create('HTMLText',$title)
 			);
 		}
@@ -1040,6 +995,8 @@ class Forum_Controller extends Page_Controller {
 	 * @return Form
 	 */
 	function AdminFormFeatures() {
+		if (!$this->canModerate()) return;
+
 		$id = (isset($this->urlParams['ID'])) ? $this->urlParams['ID'] : false;
 		
 		$fields = new FieldSet(
@@ -1077,7 +1034,11 @@ class Forum_Controller extends Page_Controller {
 		if(isset($data['ID'])) {
 			$thread = DataObject::get_by_id('ForumThread', $data['ID']);
 
-			if($thread && $thread->canEdit()) {
+			if($thread) {
+				if (!$thread->canModerate()) {
+					Security::permissionFailure($this);
+					return;
+				}
 				$form->saveInto($thread);
 				$thread->write();
 			}

commit a2c11e10f49492ab35acdccc8b82b3331766cb34

BUGFIX Allowing Post->canEdit() for admins (broken by d58cdbcb)

code/Post.php
@@ -92,12 +92,16 @@ class Post extends DataObject {
 	}
 
 	/**
-	 * Check if user can edit the post (only if it's his own)
+	 * Check if user can edit the post (only if it's his own, or he's an admin user)
 	 */
 	function canEdit() {
 		if($member = Member::currentUser()) {
+			// Admins can always edit, regardless of thread/post ownership
+			if(Permission::checkMember($member, 'ADMIN')) return true;
+
+			// Otherwise check for thread permissions and ownership
 			if($this->Thread()->canPost() && $member->ID==$this->AuthorID) return true;
-		}
+		} 
 
 		return false;
 	}

tests/ForumTest.yml
@@ -1,3 +1,6 @@
+Permission:
+	admin:
+		Code: ADMIN
 Group:
 	members:
 		Title: Forum Members
@@ -5,10 +8,10 @@ Group:
 	admins:
 		Title: Administrators
 		Code: administrators
+		Permissions: =>Permission.admin
 	special:
 		Title: Special Members who can see a forum
 		Code: special-group
-		
 Member:
 	test1:
 		Nickname: test1
@@ -53,10 +56,11 @@ Member:
 	    SuspendedUntil: 2011-10-09
 	    Groups: =>Group.special
 	suspended:
-	   
 	    SuspendedUntil: 2011-10-11
 	    Groups: =>Group.special
-		
+	admin:
+		Email: admin@example.com
+		Groups: =>Group.admins
 ForumHolder:
 	fh:
 		Title: Forum
@@ -77,7 +81,6 @@ ForumHolder:
 	    Title: Forum holder in which forums are logged-in-user-only
 	    CanViewType: LoggedInUsers
 		CanPostType: LoggedInUsers
-		
 ForumCategory:
 	fh1cat1:
 		Title: cat1
@@ -104,34 +107,28 @@ Forum:
 		CanPostType: Anyone
 		CanAttachFiles: true
 		Moderators: =>Member.moderator
-        
 	forum1cat2:
 		Title: forum1cat2
 		ParentID: =>ForumHolder.fh
 		CategoryID: =>ForumCategory.fh1cat1
 		CanAttachFiles: false
-        
 	forum2cat3:
 		Title: forum2cat3
 		ParentID: =>ForumHolder.fh2
 		CategoryID: =>ForumCategory.fh2cat3
-        
 	forum2cat4:
 		Title: forum2cat4
 		ParentID: =>ForumHolder.fh2
 		CategoryID: =>ForumCategory.fh2cat4
-        
 	forum5:
 		Title: Forum Without Category
 		ParentID: =>ForumHolder.fhNoCategories
-        
 	loggedInOnly:
 		Title: Limited to Logged In Posting and Viewing
 		ParentID: =>ForumHolder.fhPermissions
 		CanViewType: LoggedInUsers
 		CanPostType: LoggedInUsers
 		Moderators: =>Member.moderator
-        
 	limitedToGroup:
 		Title: Limited to a Specific Group
 		ParentID: =>ForumHolder.fhPermissions
@@ -140,216 +137,178 @@ Forum:
 		ViewerGroups: =>Group.special
 		PosterGroups: =>Group.special
 		Moderators: =>Member.moderator
-        
 	noPostingForum:
 		Title: This Forum is Readonly
 		ParentID: =>ForumHolder.fhPermissions
 		CanViewType: Anyone
 		CanPostType: NoOne
 		Moderators: =>Member.moderator
-
 	inheritedForum:
 		Title: This Forum is Readonly
 		ParentID: =>ForumHolder.fhPermissions
 		CanViewType: Inherit
 		CanPostType: Inherit
 		Moderators: =>Member.moderator
-		
 	inheritedForum_loggedInOnly:
 	    Title: This Forum is logged-in-user-only-inherited
 	    ParentID: =>ForumHolder.loggedInUserOnly
 	    CanViewType: Inherit
 	    CanPostType: Inherit
-	      
 ForumThread:
 	Thread1:
 		Title: Test Thread
 		NumViews: 10
 		Forum: =>Forum.general
-        
 	Thread2:
 		Title: Another Test Thread
 		NumViews: 20
 		Forum: =>Forum.general
-        
 	StickyThread1:
 		Title: Sticky Thread
 		IsSticky: true
 		Forum: =>Forum.general
-        
 	GlobalStickyThread1:
 		Title: Global Sticky Thread
 		IsGlobalSticky: true
 		Forum: =>Forum.general
-        
 	ReadonlyThread:
 		Title: Readonly Thread
 		IsReadOnly: true
 		Forum: =>Forum.general
-    
 	ThreadWhichIsInReadonlyForum:
 		Title: Thread is set to a readonly forum but you can edit this because its not readonly
 		Forum: =>Forum.noPostingForum
-
 	ThreadWhichIsInInheritedForum:
 		Title: The thread is in a forum that inherits NoOne permission for viewing and posting
 		Forum: =>Forum.inheritedForum
-		
 	ThreadCreatedBySpammer:
 		Title: Thread Created By Spammer
 		Forum: =>Forum.general
-        
 ForumThread_Subscription:
 	Subscription1: 
 		Thread: =>ForumThread.Thread1
 		Member: =>Member.test1
-    
 	Subscription2:
 		Thread: =>ForumThread.Thread1
 		Member: =>Member.test2
-
 Post:	
 	SpamFirstPost:
 		Content: SPAM SPAM SPAM
 		Thread: =>ForumThread.ThreadCreatedBySpammer
 		Author: =>Member.spammer
 		Forum: =>Forum.general
-
 	SpamSecondPost:
 		Content: SPAM SPAM SPAM
 		Thread: =>ForumThread.ThreadCreatedBySpammer
 		Author: =>Member.spammer2
 		Forum: =>Forum.general
-		
 	StickyPost:
 		Content: This is a sticky post
 		Thread: =>ForumThread.StickyThread1
 		Author: =>Member.test1
 		Forum: =>Forum.general
-
 	StickyGlobal:
 		Content: This is a global sticky
 		Thread: =>ForumThread.GlobalStickyThread1
 		Author: =>Member.test1
 		Forum: =>Forum.general
-
 	StickyReply:
 		Content: This is a reply to sticky
 		Thread: =>ForumThread.StickyThread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-
 	Post1:
 		Content: This is my first post
 		Thread: =>ForumThread.Thread1
 		Forum: =>Forum.general
 		Author: =>Member.test1
-    
 	Post2:
 		Content: This is a reply to the first post
 		Thread: =>ForumThread.Thread1
 		Forum: =>Forum.general
 		Author: =>Member.test2
-        
 	Post3:
 		Content: This is a post without a reply
 		Thread: =>ForumThread.Thread2
 		Author: =>Member.test2
 		Forum: =>Forum.general
-
 	Post4:
 		Content: This is a post to a long thread
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test1
 		Forum: =>Forum.general
-        
 	Post5:
 		Content: This is a post to a long thread
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-        
 	Post6:
 		Content: This is a post to a long thread
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-        
 	Post7:
 		Content: This is a post to a long thread
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test1
 		Forum: =>Forum.general
-        
 	Post8:
 		Content: This is a post to a long thread
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-        
 	Post9:
 		Content: This is a post to a long thread. On an 8 displayed forum. This will be the last
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-              
 	Post10:
 		Content: This is post 1 on page 2
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-
 	Post11:
 		Content: This is post 2 on page 2
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test1
 		Forum: =>Forum.general
-
 	Post12:
 		Content: This is post 3 on page 2
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-
 	Post13:
 		Content: This is post 4 on page 2
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-
 	Post14:
 		Content: This is post 5 on page 2
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test1
 		Forum: =>Forum.general
-
 	Post15:
 		Content: This is post 6 on page 2
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-
 	Post16:
 		Content: This is post 7 on page 2
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-
 	Post17:
 		Content: This is a post to a long thread. On an 8 displayed forum. This will be the last
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-
 	ReadonlyThreadPost:
 		Content: ReadonlyThreadPost
 		Thread: =>ForumThread.ReadonlyThread
 		Author: =>Member.test1
 		Forum: =>Forum.general
-            
 	Post18:
 		Content: This is the last post to a long thread
 		Thread: =>ForumThread.Thread1
 		Author: =>Member.test2
 		Forum: =>Forum.general
-

tests/PostTest.php
@@ -39,6 +39,14 @@ class PostTest extends FunctionalTest {
 		$this->assertTrue($post->canView());
 		$this->assertTrue($post->canCreate());
 		$this->assertTrue($post->canDelete());
+
+		// Admins should have full rights, even if they're not moderators
+		$admin = $this->objFromFixture('Member', 'admin');
+		$admin->logIn();
+		$this->assertTrue($post->canEdit());
+		$this->assertTrue($post->canView());
+		$this->assertTrue($post->canCreate());
+		$this->assertTrue($post->canDelete());
 	}
 	
 	function testGetTitle() {