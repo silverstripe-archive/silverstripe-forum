silverstripe-archive/silverstripe-forum

commit e4d60e9d404d27679c58a0a9d661470835cec38c

MINOR Added explicit ForumMemberProfile->show() method rather than relying on template auto detection, in order to return a proper 404 when template prerequisites like $Member aren't found

code/ForumMemberProfile.php
@@ -49,6 +49,13 @@ class ForumMemberProfile extends Page_Controller {
 		parent::init();
  	}
 
+	function show($request) {
+		$member = $this->Member();
+		if(!$member) return $this->httpError(404);
+		
+		return $this->renderWith(array('Page', 'ForumMemberProfile_show'));
+	}
+
  	/**
  	 * Get the latest 10 posts by this member
  	 */

commit fbb6250b29afd885c74835f34f2755b433169f2b

MINOR Fixed profile template usage broken by e4d60e9d

code/ForumMemberProfile.php
@@ -53,7 +53,7 @@ class ForumMemberProfile extends Page_Controller {
 		$member = $this->Member();
 		if(!$member) return $this->httpError(404);
 		
-		return $this->renderWith(array('Page', 'ForumMemberProfile_show'));
+		return $this->renderWith(array('ForumMemberProfile_show', 'Page'));
 	}
 
  	/**