29dc867eb7334627eee7fb76a9149ddd921fd632
unclecheese <unclecheese@leftandmain.com>
2014-10-02 14:57:20 +1300

    BUGFIX: RSS not working due to HTTPResponse not being returned

--- code/pagetypes/ForumHolder.php
+++ code/pagetypes/ForumHolder.php
@@ -797,7 +797,7 @@ class ForumHolder_Controller extends Page_Controller {
 				$data['last_created'], 
 				$data['last_id']
 			);
-			$rss->outputToBrowser();
+			return $rss->outputToBrowser();
     	} else {
 	
 			// Return only new posts, check the request headers!
@@ -828,7 +828,7 @@ class ForumHolder_Controller extends Page_Controller {
 					$data['last_created'],
 					$data['last_id']
 				);
-				$rss->outputToBrowser();
+				return $rss->outputToBrowser();
 			} else {
 				if($data['last_created'])
 					HTTP::register_modification_timestamp($data['last_created']);
